e9bed8d3c322be0bcb9ee1ee1552a7c223ab9a4b
amanda_k <kim.amanda@outlook.com>
2022-08-28 14:16:08 -0700

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://www.pngarts.com/files/2/Model-Transparent-Images.png",
+      title: "SUMMER SALE",
+      desc: "BEAT THE HEAT BY CHECKING OUT OUR LOWER & COOL PRICES!",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://www.pngarts.com/files/2/Model-PNG-Image-1.png",
+      title: "AUTUMN COLLECTION",
+      desc: "TAKE A PEEK ON SOME OF OUR LIMITED AUTUMN COLLECTION.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "DATE NIGHT",
+      desc: "STAND OUT FROM THE CROWD WITH OUR BOLD DRESSES WITH OUR AMAZING PRICES.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://www.pngarts.com/files/2/Tank-Top-For-Women-PNG-Free-Download.png",
+    },
+    {
+      id:2,
+      img:"https://www.pngarts.com/files/1/Cocktail-Dress-PNG-Download-Image.png",
+    },
+    {
+      id:3,
+      img:"https://www.pngarts.com/files/3/Necklace-Jewellery-Set-PNG-Transparent-Image.png",
+    },
+    {
+      id:4,
+      img:"https://www.pngarts.com/files/12/Blouse-PNG-High-Quality-Image.png",
+    },
+    {
+      id:5,
+      img:"https://www.pngarts.com/files/1/Jeans-Jacket-PNG-Photo.png",
+    },
+    {
+      id:6,
+      img:"https://www.pngarts.com/files/12/Blouse-PNG-Photo.png",
+    },
+    {
+      id:7,
+      img:"https://www.pngarts.com/files/3/Biker-Jeans-PNG-Image-Background.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
